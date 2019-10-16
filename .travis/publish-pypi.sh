e1e7d7c0b18990cde4ee231b4edb731e4feae68b
Binh Vu <binh@toan2.com>
2019-10-16 16:40:35 -0700

    update

--- .travis/publish-pypi.sh
+++ .travis/publish-pypi.sh
@@ -6,7 +6,10 @@ echo "Publish pypi package: tag=$TRAVIS_TAG os=$TRAVIS_OS_NAME"
 if [[ ! -z "${TRAVIS_TAG}" && $TRAVIS_OS_NAME = "linux" ]]; then
     pip install twine
     cd ${TRAVIS_BUILD_DIR}/pydrepr
+    ls ./
     rm -rf ./dist || echo "No previous build"
+    ls ${TRAVIS_BUILD_DIR}/pydrepr
+    ls ${TRAVIS_BUILD_DIR}/pydrepr/README.md
     python setup.py sdist
     twine upload -u __token__ -p $PYPI_PWD --skip-existing dist/*
 else
