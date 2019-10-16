9e32b038e21719ec9c1e3de7f52f600927494a46
Binh Vu <binh@toan2.com>
2019-10-15 23:54:34 -0700

    update

--- .travis/build-engine.sh
+++ .travis/build-engine.sh
@@ -10,8 +10,8 @@ if [ -f "$ENGINE_FILE" ]; then
 else
     echo ">>> Pre-built engine file does not exist. Start building it..."
     echo "List and remove previous pre-built files..."
-    ls $(python ${TRAVIS_BUILD_DIR}/pydrepr/devel info --pre_built_engine_glob)
-    rm $(python ${TRAVIS_BUILD_DIR}/pydrepr/devel info --pre_built_engine_glob)
+    ls $(python ${TRAVIS_BUILD_DIR}/pydrepr/devel info --pre_built_engine_glob) || echo "No pre-built files"
+    rm $(python ${TRAVIS_BUILD_DIR}/pydrepr/devel info --pre_built_engine_glob) || echo ""
     echo ">>> Testing..."
     echo ">>> Building..."
     python ${TRAVIS_BUILD_DIR}/pydrepr devel pylib-build -m release
