75a6b89a4b9ad9ea2ba50941ada7448d6f93d364
Binh Vu <binh@toan2.com>
2022-06-24 23:17:49 -0700

    update ci

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -9,30 +9,40 @@ set -e
 #        Empty if you want to use multiple Pythons by providing PYTHON_HOMES. This has the highest priority. If set, we won't consider PYTHON_HOMES and PYTHON_VERSIONS
 #   PYTHON_HOMES: comma-separated directories that either contains Python installations or are Python installations.
 #   PYTHON_VERSIONS: versions of Python separated by comma if you want to restricted to specific versions.
-#   TARGET: see https://doc.rust-lang.org/nightly/rustc/platform-support.html
+# Arguments:
+#   -t <target>: target platform. See https://doc.rust-lang.org/nightly/rustc/platform-support.html
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ -z "$TARGET" ]
+while getopts t: flag
+do
+    case "${flag}" in
+        t) target=${OPTARG};;
+    esac
+done
+
+if [ -z "$target" ]
 then
-    echo "TARGET is not set. https://doc.rust-lang.org/nightly/rustc/platform-support.html"
+    echo "target is not set (-t <target>). See more: https://doc.rust-lang.org/nightly/rustc/platform-support.html"
     exit 1
 fi
 
-echo "::group::Install Rust"
+echo "::group::Setup Rust"
 if ! command -v cargo &> /dev/null
 then
     # install rust and cargo
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable
     export PATH=$HOME/.cargo/bin:$PATH
 else
     echo "Rust is already installed."
     rustup show
 fi
 
-if [[ ! -d $(rustc --print target-libdir --target "$TARGET" )]]; then
-    rustup target add $TARGET
+if [[ ! -d $(rustc --print target-libdir --target "$target" ) ]]; then
+    rustup target add $target;
 fi
+
+source $HOME/.cargo/env
 echo "::endgroup::"
 echo
 
@@ -46,7 +56,7 @@ echo
 
 for PYTHON_HOME in "${PYTHON_HOMES[@]}"
 do
-    echo "::group::Building for $PYTHON_HOME"
+    echo "::group::Building for Python $PYTHON_HOME"
     
     echo "::group::Install Building Tools"
     "$PYTHON_HOME/bin/pip" install maturin
@@ -54,8 +64,8 @@ do
     echo
 
     echo "::group::Building Wheels"
-    echo "Run: $PYTHON_HOME/bin/maturin build -r -o dist -i $PYTHON_HOME/bin/python --target $TARGET"
-    "$PYTHON_HOME/bin/maturin" build -r -o dist -i "$PYTHON_HOME/bin/python" --target $TARGET
+    echo "Run: $PYTHON_HOME/bin/maturin build -r -o dist -i $PYTHON_HOME/bin/python --target $target"
+    "$PYTHON_HOME/bin/maturin" build -r -o dist -i "$PYTHON_HOME/bin/python" --target $target
     echo "::endgroup::"
 
     echo "::endgroup::"
